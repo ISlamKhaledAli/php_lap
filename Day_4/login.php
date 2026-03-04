eb49f9cb4f878e8a505d3e20c9b0783780a183fe
Islam Khaled <khaledislam9003@gmail.com>
2026-03-04 21:37:02 +0200

    add login feature

---
+++ Day_4/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<title>Login</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div class="container mt-5">
+
+<div class="card shadow p-4">
+
+<h3>Login</h3>
+
+<form action="check_login.php" method="POST">
+
+<input type="text" name="username" class="form-control mb-3" placeholder="Username">
+
+<input type="password" name="password" class="form-control mb-3" placeholder="Password">
+
+<button class="btn btn-success">Login</button>
+
+</form>
+
+</div>
+
+</div>
+
+</body>
+
+</html>
